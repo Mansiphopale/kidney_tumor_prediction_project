5d71e99a8a053578add56680c07d8a5f2d823432
Mansiphopale <phopalemansi@gmail.com>
2026-04-05 14:16:40 +0530

    Initial commit using py310 environment

--- src/cnnClassifier/pipeline/stage_04_model_eva_pipeline.py
+++ src/cnnClassifier/pipeline/stage_04_model_eva_pipeline.py
@@ -11,10 +11,11 @@ STAGE_NAME = "Evaluation stage"
 
 class EvaluationPipeline:
     def __init__(self):
-        os.environ["MLFLOW_TRACKING_URI"]="https://dagshub.com/Mansiphopale/kidney_tumor_prediction_project.mlflow"
-        # Set your DagsHub credentials (MLflow uses HTTP Basic Auth)
-        os.environ["MLFLOW_TRACKING_USERNAME"] = "Mansiphopale"
-        os.environ["MLFLOW_TRACKING_PASSWORD"] = "deeb02e71df1418bbf4e0e119b8058fc4774e721"
+        pass
+        # os.environ["MLFLOW_TRACKING_URI"]="https://dagshub.com/Mansiphopale/kidney_tumor_prediction_project.mlflow"
+        # # Set your DagsHub credentials (MLflow uses HTTP Basic Auth)
+        # os.environ["MLFLOW_TRACKING_USERNAME"] = "Mansiphopale"
+        # os.environ["MLFLOW_TRACKING_PASSWORD"] = "deeb02e71df1418bbf4e0e119b8058fc4774e721"
 
 
     def main(self):
@@ -23,7 +24,7 @@ class EvaluationPipeline:
         evaluation = Evaluation(eval_config)
         evaluation.evaluation()
         evaluation.save_score()
-        evaluation.log_into_mlflow()
+        #evaluation.log_into_mlflow()
 
 
 
